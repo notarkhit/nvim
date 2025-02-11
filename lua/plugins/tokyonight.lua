e95e37c1e30b80bc802acc52c93cfd566b67b5c6
notarkhit <notarkhit@gmail.com>
2025-02-11 23:16:12 +0530

    update theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,6 +4,10 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+	    require("tokyonight").setup({
+		style =  "night",
+		transparent = true
+	    })
       -- load the colorscheme here
       vim.cmd([[colorscheme tokyonight-night]])
       vim.cmd.hi("Comment gui=none")
