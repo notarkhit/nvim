6e5165967d2c30de9f21a7c74c24a15945e69ab6
notarkhit <notarkhit@gmail.com>
2025-02-12 14:11:54 +0530

    add highlight on yank

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,7 +1,8 @@
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = augroup("highlight_yank"),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+  end,
 })
+
