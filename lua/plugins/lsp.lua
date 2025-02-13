2cea669c01e6011e435acf58ad46276b66d2650f
notarkhit <notarkhit@gmail.com>
2025-02-13 16:31:14 +0530

    update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,4 +8,18 @@ return {
 	  },
 	},
   },
+
+  { "Bilal2453/luvit-meta", lazy = true },
+  
+  {
+  --[[ Main lsp configuration ]]
+	"neovim/nvim-lspconfig",
+	dependencies = {
+	  { "williamboman/mason.nvim", config = true },
+	  "williamboman/mason-lspconfig.nvim",
+	  "WhoIsSethDaniel/mason-tool-installer.nvim",
+	  { "j-hui/fidget.nvim", opts = {} },
+	  "hrsh7th/cmp-nvim-lsp",
+	}
+  }
 }
