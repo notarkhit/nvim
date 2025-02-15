f25b4fc213b0d3a012d2042495cb594908872f5c
notarkhit <notarkhit@gmail.com>
2025-02-15 12:55:34 +0530

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,5 +12,20 @@ return {
 		end,
 	},
 	{ "nvim-telescope/telescope-ui-select.nvim" },
-  }
+	{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+  },
+  config = function()
+	require("telescope").setup({
+	  extensions = {
+		["ui-select"] = {
+		  require("telescope.themes").get_dropdown(),
+		},
+	  },
+	})
+
+	pcall(require("telescope").load_extension, "fzf")
+	pcall(require("telescope").load_extension, "ui-select")
+
+
+  end
 }
