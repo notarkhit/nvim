141d25b443d6040b1da279ae29bf368ca6b7743b
notarkhit <notarkhit@gmail.com>
2025-02-16 13:49:05 +0530

    update notify.lua

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -14,8 +14,9 @@ return {
 			minimum_width = 50,
 			render = "compact",
 			stages = "fade_in_slide_out",
-			timeout = 50,
-			level = require("notify").level,
+			timeout = 500,
+			merge_duplicates = true,
+			top_down = false,
 		})
 		vim.notify = require("notify")
 		vim.keymap.set("n", "<leader>nd", require("notify").dismiss, { noremap = true, silent = true })
