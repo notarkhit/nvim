59ff54a302a78cb748f5d686e1c8b463ba95bbb2
notarkhit <notarkhit@gmail.com>
2025-03-14 19:29:50 +0530

    configure render markdown plugin

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -5,5 +5,19 @@ return {
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		highlight = {
+			enable = true, -- Enable syntax highlighting
+			groups = {
+				-- Example: Customize or disable specific highlights
+				Heading = { link = "Title" },
+				Code = { link = "Normal" },
+				BlockQuote = { link = "Comment" },
+				HorizontalRule = { link = "Comment" },
+				Table = { link = "Normal" },
+				-- Remove background highlight for normal text
+				Normal = { bg = "NONE" },
+			},
+		},
+	},
 }
