3a3d9c09f97c4fb4158976cf352c41f5465d39bf
notarkhit <notarkhit@gmail.com>
2025-03-14 19:54:42 +0530

    render md config

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -3,10 +3,7 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
-	require("render-markdown").setup({
-		render_modes = { "n", "c", "t" },
-	}),
+	opts = {
+		render_modes = { "n", "c", "t", "i" },
+	},
 }
