600ed2b77a844c71d47348377e8b086c6d923c81
notarkhit <notarkhit@gmail.com>
2025-03-23 18:44:48 +0530

    update render_markdown.lua

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -4,6 +4,6 @@ return {
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	opts = {
-		render_modes = { "n", "c", "t", "i" },
+		render_modes = { "n", "c", "t" },
 	},
 }
