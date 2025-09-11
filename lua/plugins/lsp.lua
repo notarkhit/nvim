8c7db37422f43b3c6852c6bc5a0f3d989f0a528e
notarkhit <notarkhit@gmail.com>
2025-09-11 20:21:51 +0530

    fix lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,144 +1,71 @@
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		{
+			"mason-org/mason-lspconfig.nvim",
+			opts = {
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"pyright",
+					"bashls",
+					"hyprls",
+					"emmet_ls",
+					"ts_ls",
+					"cssls",
+					"jdtls",
+					"rust_analyzer",
+				},
 			},
 		},
+		"hrsh7th/cmp-nvim-lsp",
+		{ "j-hui/fidget.nvim",    opts = {} }, -- optional status UI
 	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	{ "Bilal2453/luvit-meta", lazy = true },
+		-- 🗝️ Common keymaps + behavior
+		local on_attach = function(_, bufnr)
+			local map = function(keys, func, desc)
+				vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+			end
+			map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+			map("K", vim.lsp.buf.hover, "Hover Docs")
+			map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+			map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+		end
 
-	{
-		--[[ Main lsp configuration ]]
-		"neovim/nvim-lspconfig",
-		event = "BufReadPre",
-		dependencies = {
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			{ "j-hui/fidget.nvim", opts = {} },
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-				callback = function(event)
-					local map = function(keys, func, desc, mode)
-						mode = mode or "n"
-						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
-					)
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-							end,
-						})
-					end
-
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-						end, "[T]oggle Inlay [H]ints")
-					end
-				end,
-			})
-
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-			local servers = {
-				lua_ls = {
+		-- 📦 Servers with optional custom settings
+		local servers = {
+			lua_ls = {
+				settings = {
 					Lua = {
-						runtime = {
-							version = "LuaJIT",
-							path = vim.split(package.path, ";"),
-						},
-						diagnostics = {
-							enable = true,
-							global = { "vim" },
-						},
-						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
-							checkThirdParty = false,
-						},
-						telemetry = {
-							enable = false,
-						},
+						diagnostics = { globals = { "vim" } },
+						workspace = { checkThirdParty = false },
 					},
 				},
-			}
+			},
+			pyright = {},
+			ts_ls = {},
+			rust_analyzer = {},
+			clangd = {},
+			bashls = {},
+			cssls = {},
+			html = {},
+			emmet_ls = {},
+			jdtls = {},
+			hyprls = {},
+		}
 
-			-- [[ Mason ]] --
-			require("mason").setup()
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua",
-				"pyright",
-				"clangd",
-				"lua-language-server",
-				"bashls",
-				"hyprls",
-				"emmet-ls",
-				"typescript-language-server",
-				"css-lsp",
-				"jdtls",
-			})
-
-			require("mason-tool-installer").setup({
-				ensure_installed = ensure_installed,
-				auto_update = true,
-				run_on_start = true,
-				integrations = {
-					["mason-lspconfig"] = true,
-					["mason-null-ls"] = true,
-					["mason-nvim-dap"] = true,
-				},
-			})
-
-			require("mason-lspconfig").setup({
-				ensure_installed = {},
-				automatic_installation = true,
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
-		end,
-	},
+		--  Setup all servers
+		for name, opts in pairs(servers) do
+			opts.capabilities = capabilities
+			opts.on_attach = on_attach
+			lspconfig[name].setup(opts)
+		end
+	end,
 }
