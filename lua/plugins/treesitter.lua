7a8d6881757ab3b2968368cf41106173e6b1565a
notarkhit <notarkhit@gmail.com>
2025-09-11 21:57:55 +0530

    fix treesitter opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,37 +2,38 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"c",
-					"bash",
-					"diff",
-					"html",
-					"lua",
-					"luadoc",
-					"vim",
-					"vimdoc",
-					"query",
-					"markdown",
-					"markdown_inline",
-				},
-				sync_install = false,
-				auto_install = true,
-				ignore_install = { "javascript" },
-				highlight = {
-					enable = true,
-					disable = { "rust" },
-					disable = function(lang, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-					end,
-					additional_vim_regex_highlighting = true,
-				},
-			})
-		end,
+		opts = {
+			ensure_installed = {
+				"c",
+				"bash",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			auto_install = true,
+			ignore_install = { "javascript" },
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
+					if lang == "rust" then
+						return true
+					end
+					local max_filesize = 100 * 1024 -- 100 KB
+					---@diagnostic disable-next-line: undefined-field
+					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+				additional_vim_regex_highlighting = true,
+			},
+		}
 	},
 }
