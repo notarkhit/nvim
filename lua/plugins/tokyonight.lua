1a7850f6cf3454fc676263d379ca9f097e3521a6
notarkhit <notarkhit@gmail.com>
2026-01-08 09:03:53 +0530

    [Fix] tokyonight theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,18 +1,22 @@
 return {
-	-- the colorscheme should be available when starting Neovim
 	"folke/tokyonight.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			transparent = true,
-			on_highlights = function(hl, colors)
-				hl.Comment = { fg = "#979baf" }
-			end,
-		})
-		-- load the colorscheme here
-		vim.cmd([[colorscheme tokyonight-night]])
+	lazy = false,
+	priority = 1000,
+
+	opts = {
+		style = "night",
+		transparent = true,
+
+		on_colors = function(_) end,
+
+		on_highlights = function(hl, _)
+			hl.Comment = { fg = "#979baf" }
+		end,
+	},
+
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd.colorscheme("tokyonight-night")
 		vim.cmd.hi("Comment gui=none")
 	end,
 }
