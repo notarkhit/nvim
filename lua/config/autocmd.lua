44995f51bcc5b08e4bcdf743ca6be70d9f8c3251
notarkhit <notarkhit@gmail.com>
2026-01-08 09:03:19 +0530

    [Fix] hyprls autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -7,16 +7,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Hyprlang LSP
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
 	pattern = { "*.hl", "hypr*.conf" },
 	callback = function(event)
 		print(string.format("starting hyprls for %s", vim.inspect(event)))
-		vim.lsp.start({
+		vim.lsp.start {
 			name = "hyprlang",
 			cmd = { "hyprls" },
 			root_dir = vim.fn.getcwd(),
-		})
-	end,
+			settings = {
+				hyprls = {
+					preferIgnoreFile = true, -- set to false to prefer `hyprls.ignore`
+					ignore = { "hyprlock.conf", "hypridle.conf" }
+				}
+			}
+		}
+	end
 })
 
 vim.filetype.add({
