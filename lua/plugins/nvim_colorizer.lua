6b7ed81a38d99b2c97b18bda177b712cde9d4ff4
notarkhit <notarkhit@gmail.com>
2026-01-09 18:58:11 +0530

    [Update] fix colorizer

--- lua/plugins/nvim_colorizer.lua
+++ lua/plugins/nvim_colorizer.lua
@@ -1,6 +1,6 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	config = function()
-		require("colorizer").setup()
-	end,
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = {   -- set to setup table
+	},
 }
