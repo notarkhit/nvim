801519692def210e3e59105e370d98e6e75c2e2a
notarkhit <notarkhit@gmail.com>
2026-01-10 23:51:21 +0530

    [Update] disable hyprlang lsp start message

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
 	pattern = { "*.hl", "hypr*.conf" },
 	callback = function(event)
-		print(string.format("starting hyprls for %s", vim.inspect(event)))
+		-- print(string.format("starting hyprls for %s", vim.inspect(event)))
 		vim.lsp.start {
 			name = "hyprlang",
 			cmd = { "hyprls" },
