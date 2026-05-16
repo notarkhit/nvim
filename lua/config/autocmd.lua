06c725fb9d8d878d0fba59b1653125bb54ddbcab
notarkhit <notarkhit@gmail.com>
2026-05-16 14:27:50 +0530

    remove hyprlang(deprecated) lsp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -5,26 +5,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
 	end,
 })
-
--- Hyprlang LSP
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-	pattern = { "*.hl", "hypr*.conf" },
-	callback = function(event)
-		-- print(string.format("starting hyprls for %s", vim.inspect(event)))
-		vim.lsp.start {
-			name = "hyprlang",
-			cmd = { "hyprls" },
-			root_dir = vim.fn.getcwd(),
-			settings = {
-				hyprls = {
-					preferIgnoreFile = true, -- set to false to prefer `hyprls.ignore`
-					ignore = { "hyprlock.conf", "hypridle.conf" }
-				}
-			}
-		}
-	end
-})
-
-vim.filetype.add({
-	pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-})
